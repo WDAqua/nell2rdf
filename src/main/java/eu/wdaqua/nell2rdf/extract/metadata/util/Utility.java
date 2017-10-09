489443097dc427128f02bc1fe4e149e06acb708e
Maísa Duarte <maisa.duarte@gmail.com>
2017-10-09 12:26:35 +0200

    SpreadsheetEdits Regex Addiction

--- src/main/java/eu/wdaqua/nell2rdf/extract/metadata/util/Utility.java
+++ src/main/java/eu/wdaqua/nell2rdf/extract/metadata/util/Utility.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package eu.wdaqua.nell2rdf.extract.metadata.util;
 
 import eu.wdaqua.nell2rdf.extract.metadata.models.LineInstanceJOIN;
