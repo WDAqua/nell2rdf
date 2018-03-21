ee4935c0d30c243751972018a3c1af49f87c0f12
José M. Giménez-García <jm.gimenez.garcia@gmail.com>
2018-03-21 14:11:09 +0100

    Added script to download all NELL files

---
+++ src/main/resources/downloadNellFiles.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+set -x
+
+for i in ${seq $2 $3};
+do
+	wget -c -P "$1" "http://rtw.ml.cmu.edu/resources/results/08m/NELL.08m.$i.esv.csv.gz"
+	wget -c -P "$1" "http://rtw.ml.cmu.edu/resources/results/08m/NELL.08m.$i.esv.csv.gz"
+	wget -c -P "$1" "http://rtw.ml.cmu.edu/resources/results/08m/NELL.08m.$i.cesv.csv.gz"
+done
