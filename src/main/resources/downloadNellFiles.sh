5fbaa1ed7322df7b0bb737b8b656944f41d93311
José M. Giménez-García <jose.gimenez.garcia@univ-st-etienne.fr>
2018-03-21 09:14:09 -0500

    bugfixes in downloadNellFiles.sh

--- src/main/resources/downloadNellFiles.sh
+++ src/main/resources/downloadNellFiles.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
-
-set -x
-
-for i in ${seq $2 $3};
-do
+set -x 
+for i in $(seq $2 $3); do 
 	wget -c -P "$1" "http://rtw.ml.cmu.edu/resources/results/08m/NELL.08m.$i.esv.csv.gz"
 	wget -c -P "$1" "http://rtw.ml.cmu.edu/resources/results/08m/NELL.08m.$i.esv.csv.gz"
 	wget -c -P "$1" "http://rtw.ml.cmu.edu/resources/results/08m/NELL.08m.$i.cesv.csv.gz"
 done
+
