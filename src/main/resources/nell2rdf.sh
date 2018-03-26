eb5f7bc19e7df64a5b5ae165917ce56fa90bfb93
José M. Giménez-García <jose.gimenez.garcia@univ-st-etienne.fr>
2018-03-26 06:29:59 -0500

    Bugfixes in nell2rdf.sh

--- src/main/resources/nell2rdf.sh
+++ src/main/resources/nell2rdf.sh
@@ -6,7 +6,7 @@ declare -a annotationextensions=(nt reif.nt nary.nt sp.nt nd.nt nq)
 # Defaults
 declare jarFile="../../../target/NellConverter-0.0.1-SNAPSHOT.jar"
 declare serializationFormat="N-TRIPLE"
-declare javaArguments=""
+declare javaArguments
 declare compress=""
 
 function showhelp {
@@ -17,14 +17,15 @@ function showhelp {
 	echo
 	echo "  OPTIONS:"
 	echo "    -c, --compress  compress the output in the desired format (gzip|bzip|lzop)"
-	echo "    -f, --format    serialization format to encode the RDF triples (default N-TRIPLES)""
+	echo "    -f, --format    serialization format to encode the RDF triples (default N-TRIPLES)"
 	echo "                            (RDF/XML | RDF/XML-ABBREV | N-TRIPLE | TURTLE | TTL | N3 )"
 	echo "    -h, --help      shows this help and exists"
 	echo "    -j, --jar       path to NELL2RDF java jar file (default $jarFile)"
-	echo "    -p, --params    parameters for java command (i.e., -Xmx40g)""
-	echo "  ONTOLOGY    path to NELL's ontology file"
-	echo "  PROMOTED    path to NELL's promoted beliefs file"
-	echo "  CANDIDATES  path to NELL's candidates file" 
+	echo "    -p, --params    parameters for java command (i.e., -Xmx40g)"
+	echo "  ONTOLOGY    path to NELL\'s ontology file"
+	echo "  PROMOTED    path to NELL\'s promoted beliefs file"
+	echo "  CANDIDATES  path to NELL\'s candidates file"
+	echo
 }
 
 function decompress { 
@@ -46,8 +47,8 @@ function compress {
 }
 
 function n2r { # jarFile serializationFormat annotationFormat annotationExtension ontologyFile dataFile javaArguments
-	java "$7" -jar "$1" "$2" $3 "$5" "$5".ttl "$6" "$6".$4
-	LC_ALL=C sort -u -o "$6".$4 "$6".$4
+	java $7 -jar "$1" "$2" "$3" "$5" "$5".ttl "$6" "$6"."$4"
+	LC_ALL=C sort -u -o "$6"."$4" "$6"."$4"
 	echo "Generation of $6.$4 successful"
 }
 
@@ -56,8 +57,8 @@ function n2r { # jarFile serializationFormat annotationFormat annotationExtensio
 getopt --test > /dev/null
 if [[ $? -eq 4 ]]; then
     # enhanced getopt works
-    OPTIONS=c::f:hj:p:
-	LONGOPTIONS=compress::,format:,help,jar:,params:
+    OPTIONS=c:f:hj:p:
+	LONGOPTIONS=compress:,format:,help,jar:,params:
     COMMAND=$(getopt -o $OPTIONS -l $LONGOPTIONS -n "$0" -- "$@")
     if [[ $? -ne 0 ]]; then
     	exit 2
@@ -70,6 +71,7 @@ fi
 while true; do
 	case "$1" in
 		-c|--compress)
+			echo "uno $1 dos $2"
 			case "$2" in
 				""|"gz"|"gzip") compress=gzip ; shift 2 ;;
 				"bz"|"bz2"|"bzip"|"bzip2") compress=bzip ; shift 2 ;;
@@ -86,6 +88,7 @@ while true; do
 			shift 2
 			;;
 		-p|--params)
+			echo "uno $1 dos $2"
 			javaArguments="$2"
 			shift 2
 			;;
@@ -99,6 +102,12 @@ while true; do
 			;;
 	esac
 done
+
+if [[ "$#" -ne 3 ]]; then
+	echo "Illegal number of parameters ($)"
+	echo "For help, type $0 --help"
+	exit 1
+fi
 		
 declare ontologyFile="$1"
 declare promotedFile="$2"
@@ -106,21 +115,25 @@ declare candidatesFile="$3"
 
 set -x
 
-decompress "$ontologyFile"
-decompress "$promotedFile"
-decompress "$candidatesFile"
+decompress "$ontologyFile" &
+decompression[0]=$!
+decompress "$promotedFile" &
+decompression[1]=$!
+decompress "$candidatesFile" &
+decompression[2]=$!
+wait ${decompression[0]} ${decompression[1]} ${decompression[2]}
 
 for i in ${!annotationformats[@]}; do
-	n2r "$jarFile" ${serializationFormat[$i] "$annotationFormat" ${ontologyFile%.*} ${promotedFile%.*} "$javaArguments" &
+	n2r "$jarFile" "$serializationFormat" ${annotationformats[$i]} ${annotationextensions[$i]} ${ontologyFile%.*} ${promotedFile%.*} "$javaArguments" &
 	promoted[$i]=$!
-	n2r "$jarFile" ${serializationFormat[$i] "$annotationFormat" ${ontologyFile%.*} ${candidatesFile%.*} "$javaArguments" &
+	n2r "$jarFile" "$serializationFormat" ${annotationformats[$i]} ${annotationextensions[$i]} ${ontologyFile%.*} ${candidatesFile%.*} "$javaArguments" &
 	candidates[$i]=$!
 	wait ${promoted[$i]} ${candidates[$i]}
-	LC_ALL=C sort -u -o ${promotedFile%.*}${candidatesFile%.*}."$annotationextensions[$i]" "$promotedFile"."$annotationextensions[$i]" "$candidatesFile"."$annotationextensions[$i]"
-	echo "Generation of ${promotedFile%.*}${candidatesFile%.*}.$annotationextensions[$i] successful"
-	compress "$promotedFile"."$annotationextensions[$i]" &
-	compress "$candidatesFile"."$annotationextensions[$i]" &
-	compress ${promotedFile%.*}${candidatesFile%.*}."$annotationextensions[$i]" &
+	LC_ALL=C sort -u -o ${promotedFile%.*}${candidatesFile##*/%.*}.${annotationextensions[$i]} "$promotedFile".${annotationextensions[$i]} "$candidatesFile".${annotationextensions[$i]}
+	echo "Generation of ${promotedFile%.*}${candidatesFile##*%.*}.${annotationextensions[$i]} successful"
+	compress "$promotedFile".${annotationextensions[$i]} &
+	compress "$candidatesFile".${annotationextensions[$i]} &
+	compress ${promotedFile%.*}${candidatesFile%.*}.${annotationextensions[$i]} &
 done
 
 echo "Deleting NELL decompressed files"
